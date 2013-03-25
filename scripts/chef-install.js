5dd1ad35d27be0e8ce78e42925775f78b02a9ea2
Kazutoshi Nakagawa <jewel.lapislazuli@gmail.com>
2013-03-25 17:20:00 +0900

    chef-clientのインストールをチェックするメソッドを追加

--- scripts/chef-install.js
+++ scripts/chef-install.js
@@ -5,14 +5,31 @@ function installChefClient(shell, installer){
     return result;
 }
 
-function getFileObject(fso, installerPath){
-    var installer = fso.GetFile(installerPath);
+function getFileObject(fso, filePath){
+    var file = fso.GetFile(filePath);
 
-    if( installer.FileExists == false ){
+    if (file.FileExists == false){
         return null;
     }
 
-    return installer;
+    return file;
+}
+
+function checkChefInstalled(fso, shell){
+    var chefClientPath = "C:\\opscode\\chef\\bin\\chef-solo.bat";
+    var chefClient = getFileObject(fso, chefClientPath);
+
+    if (chefClient == null){
+        return false;
+    }
+
+    var result = shell.Run(chefClient.Path + " -version", 0, true);
+
+    if (result == 0){
+        return true;
+    } else{
+        return false;
+    }
 }
 
 function main(){
@@ -20,15 +37,25 @@ function main(){
     var fso = WScript.CreateObject("Scripting.FileSystemObject");
     var shell = WScript.CreateObject("WScript.Shell");
 
+    if (checkChefInstalled(fso, shell) == true){
+        WScript.Quit(1);
+    }
+
     installerPath = args(0);
 
     var installer = getFileObject(fso, installerPath);
-    if( installer == null ){
+    if (installer == null){
         WScript.Quit(1);
     }
 
     var result = installChefClient(shell, installer);
-    if( result != 0 ){
+    if (result != 0){
+        WScript.Quit(1);
+    }
+
+    if (checkChefInstalled(fso, shell) == true){
+        return
+    } else {
         WScript.Quit(1);
     }
 }
